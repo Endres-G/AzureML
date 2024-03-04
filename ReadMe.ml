7b4204e0b402127c2b8a44e83f29b1ecf2c73bf3
Gabriel Endres <110736873+Endres-G@users.noreply.github.com>
2024-03-04 14:15:21 -0300

    Update ReadMe.ml

--- ReadMe.ml
+++ ReadMe.ml
@@ -1,1 +1,14 @@
-.
+{
+  "tipo_de_tarefa": "Regressão",
+  "métrica_primária": "Erro de quadrado de média de raiz normalizado",
+  "explicar_o_melhor_modelo": false,
+  "modelos_permitidos": ["RandomForest", "LightGBM"],
+  "modelos_bloqueados": ["TensorFlowDNN", "TensorFlowLinearRegressor"],
+  "número_de_validações_cruzadas": null,
+  "aprendizado_profundo": false,
+  "tempo_de_treinamento_horas": 0.25,
+  "limite_de_pontuação_da_métrica": 0.085,
+  "tipo_de_validação": "Divisão de validação de treinamento",
+  "iteração_simultânea_máxima": 3
+}
+
